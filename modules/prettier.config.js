da5eeca03fed337822bfe698e53f76ee544d8fb7
sam bacha <sam@manifoldfinance.com>
2022-06-21 13:25:21 -0700

    v1.3.0

--- modules/prettier.config.js
+++ modules/prettier.config.js
@@ -1,24 +1,45 @@
 /**
- * @file Prettier configuration for Conformance
- * @version 1.2.0
- * @summary base config adapted from AirBNB to maximize performance
+ * @exports prettier-config-conformance
+ * @version 1.3.0
+ * @summary base config adapted from AirBNB to reduce AST Diff Churn
  * @schema http://json.schemastore.org/prettierrc
  */
 
 'use strict';
 
-module.exports = {
-  arrowParens: 'always',
-  bracketSpacing: true,
-  endOfLine: 'lf',
-  jsxBracketSameLine: false,
-  jsxSingleQuote: false,
-  printWidth: 100,
-  proseWrap: 'always',
-  quoteProps: 'as-needed',
-  semi: true,
-  singleQuote: true,
-  tabWidth: 2,
-  trailingComma: 'all',
-  useTabs: false,
-};
+// @ts-check
+/**
+ * @type {import('prettier').Options}
+ */
+ module.exports = {
+   arrowParens: 'always',
+   bracketSameLine: true,
+   bracketSpacing: false,
+   embeddedLanguageFormatting: 'auto',
+   htmlWhitespaceSensitivity: 'strict',
+   insertPragma: false,
+   jsxSingleQuote: false,
+   printWidth: 110,
+   proseWrap: 'always',
+   quoteProps: 'consistent',
+   requirePragma: false,
+   semi: true,
+   singleQuote: true,
+   tabWidth: 2,
+   trailingComma: 'all',
+   useTabs: false,
+   vueIndentScriptAndStyle: false,
+   plugins: [require.resolve("@trivago/prettier-plugin-sort-imports")],
+   importOrder: [
+     "^(react|react-dom)$",
+     "^next.+$",
+     "^@.+$",
+     "^[^\\/]+$",
+     "^.+\\.(svg|png|jpg|jpeg|webp)$",
+     "^.+\\/.+$",
+     "^[./]",
+     "^[../]",
+   ],
+   importOrderSeparation: true,
+   importOrderSortSpecifiers: true,
+ };
